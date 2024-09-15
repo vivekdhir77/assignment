022619ebd016fee14d95c8642e5c803415ab7c26
vivekdhir77 <vivekdhir77@gmail.com>
2024-09-15 09:03:10 -0400

    ordered lists

--- kanbas-react-web-app/src/Labs/Lab1/index.tsx
+++ kanbas-react-web-app/src/Labs/Lab1/index.tsx
@@ -28,6 +28,21 @@ tag to tell browsers to render the gaps.
         </p>
 
 </div>
+<div id="wd-lists">
+  <h4>List Tags</h4>
+  <h5>Ordered List Tag</h5>
+  How to make pancakes:
+  <ol id="wd-pancakes">
+    <li>Mix dry ingredients.</li>
+    <li>Add wet ingredients.</li>
+    <li>Stir to combine.</li>
+    <li>Heat a skillet or griddle.</li>
+    <li>Pour batter onto the skillet.</li>
+    <li>Cook until bubbly on top.</li>
+    <li>Flip and cook the other side.</li>
+    <li>Serve and enjoy!</li>
+  </ol>
+</div>
 
       </div>
     );
