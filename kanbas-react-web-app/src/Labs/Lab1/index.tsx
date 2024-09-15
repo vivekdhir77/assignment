b5136c9c6e802a2f34b01e2a744c727002f33d07
vivekdhir77 <vivekdhir77@gmail.com>
2024-09-15 09:02:45 -0400

    paragraph part

--- kanbas-react-web-app/src/Labs/Lab1/index.tsx
+++ kanbas-react-web-app/src/Labs/Lab1/index.tsx
@@ -2,7 +2,35 @@ export default function Lab1() {
     return (
       <div id="wd-lab1">
         <h2>Lab 1</h2>
+        <h3>HTML Examples</h3>
+        <div id="wd-h-tag">
+          <h4>Heading Tags</h4>
+          <div id="wd-p-tag">
+  <h4>Paragraph Tag</h4>
+    <p id="wd-p-1">
+This is a paragraph. We often separate a long set of sentences with vertical spaces to make the text easier to read. Browsers ignore vertical white spaces and render all the text as one single set of sentences. To force the browser to add vertical spacing, wrap the paragraphs you want to separate with the paragraph tag
+  </p>
+</div>
+
+<p id="wd-p-2">
+This is the first paragraph. The paragraph tag is used to format
+vertical gaps between long pieces of text like this one.
+        </p>
+        <p id="wd-p-3">
+This is the second paragraph. Even though there is a deliberate white
+gap between the paragraph above and this paragraph, by default
+browsers render them as one contiguous piece of text as shown here on
+the right.
+        </p>
+        <p id="wd-p-4">
+This is the third paragraph. Wrap each paragraph with the paragraph
+tag to tell browsers to render the gaps.
+        </p>
+
+</div>
+
       </div>
     );
   }
+
   
