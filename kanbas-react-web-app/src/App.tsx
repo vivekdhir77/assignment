24968cff8daf197f376cc67f23594e2d1877ef89
vivekdhir77 <vivekdhir77@gmail.com>
2024-09-16 10:34:15 -0400

    Almost final

--- kanbas-react-web-app/src/App.tsx
+++ kanbas-react-web-app/src/App.tsx
@@ -1,4 +1,5 @@
 import Labs from "./Labs";
+import Kanbas from "./Kanbas";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/Labs" />} />
           <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
       </div>
     </HashRouter>
---
+++ kanbas-react-web-app/src/Kanbas/Courses/Assignments/index.tsx
@@ -0,0 +1,36 @@
+export default function Assignments() {
+    return (
+      <div id="wd-assignments">
+        <input id="wd-search-assignment"
+               placeholder="Search for Assignments" />
+        <button id="wd-add-assignment-group">+ Group</button>
+        <button id="wd-add-assignment">+ Assignment</button>
+        <h3 id="wd-assignments-title">
+          ASSIGNMENTS 40% of Total <button>+</button>
+        </h3>
+        <ul id="wd-assignment-list">
+          <li className="wd-assignment-list-item">
+            <a className="wd-assignment-link"
+              href="#/Kanbas/Courses/1234/Assignments/123">
+              A1 - ENV + HTML
+            </a>
+            <p>Due Tuesday 17th sept</p>
+          </li>
+          <li className="wd-assignment-list-item">
+            <a className="wd-assignment-link"
+              href="#/Kanbas/Courses/1234/Assignments/123">
+              A2 - CSS
+            </a>
+            <p>Due Wednesday 17th Oct</p>
+          </li>
+          <li className="wd-assignment-list-item">
+            <a className="wd-assignment-link"
+              href="#/Kanbas/Courses/1234/Assignments/123">
+              A3 - JS
+            </a>
+            <p>Due Thursday 21th Oct</p>
+          </li>
+        </ul>
+      </div>
+  );}
+  
