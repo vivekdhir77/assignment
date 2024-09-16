3eb6653a7d71b6f6e4708c1d08e31cca3f158bba
vivekdhir77 <vivekdhir77@gmail.com>
2024-09-15 22:24:01 -0400

    table

--- kanbas-react-web-app/src/Labs/Lab1/index.tsx
+++ kanbas-react-web-app/src/Labs/Lab1/index.tsx
@@ -68,6 +68,46 @@ Your favorite books (in no particular order)
 
 </div>
 
+<div id="wd-tables">
+        <h4>Table Tag</h4>
+        <table border={1} width="100%">
+          <thead>
+            <tr>
+              <th>Quiz</th>
+              <th>Topic</th>
+              <th>Date</th>
+              <th>Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Q1</td>
+              <td>HTML</td>
+              <td>2/3/21</td>
+              <td>85</td>
+            </tr>
+            <tr>
+              <td>Q2</td>
+              <td>CSS</td>
+              <td>2/10/21</td>
+              <td>90</td>
+            </tr>
+            <tr>
+              <td>Q3</td>
+              <td>JavaScript</td>
+              <td>2/17/21</td>
+              <td>95</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={3}>Average</td>
+              <td>90</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+
       </div>
     );
   }
