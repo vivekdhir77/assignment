4d52977148f4fa38674e63a9e84c436a8f55b944
vivekdhir77 <vivekdhir77@gmail.com>
2024-09-16 10:45:45 -0400

    Modified Course Status

--- kanbas-react-web-app/src/Kanbas/Courses/Home/Status.tsx
+++ kanbas-react-web-app/src/Kanbas/Courses/Home/Status.tsx
@@ -1,10 +1,21 @@
 export default function CourseStatus() {
     return (
       <div id="wd-course-status">
-        <h2>Course Status</h2>
-        <button>Unpublish</button> <button>Publish</button>
-        {/* Complete on your own */}
-        <button>View Course Notifications</button>
+        <div>
+            <h2>Course Status</h2>
+            <button>Unpublish</button> 
+            <button>Publish</button>
+        </div>
+        <br />
+        <div>
+            <button>Import Existing Contents</button><br />
+            <button>Import From Commons</button><br />
+            <button>Choose Home Page</button><br />
+            <button>View Course Stream</button><br />
+            <button>New Announcement</button><br />
+            <button>New Analytics</button><br />
+            <button>View Course Notifications</button>
+        </div>
       </div>
   );}
   
