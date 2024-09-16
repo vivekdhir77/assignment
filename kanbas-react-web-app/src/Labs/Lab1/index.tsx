8d3fe7469d6b9ce5b1dc168221962f625855971c
vivekdhir77 <vivekdhir77@gmail.com>
2024-09-15 22:45:53 -0400

    image

--- kanbas-react-web-app/src/Labs/Lab1/index.tsx
+++ kanbas-react-web-app/src/Labs/Lab1/index.tsx
@@ -108,6 +108,20 @@ Your favorite books (in no particular order)
         </table>
       </div>
 
+      <div id="wd-images">
+  <h4>Image tag</h4>
+  Loading an image from the internet:
+  <br />
+  <img id="wd-starship"
+    width="400px"
+   src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+  />
+  <br />
+  Loading a local image:
+  <br />
+  <img id="wd-teslabot" src="teslabot.png" height="200px" />
+</div>
+
       </div>
     );
   }
