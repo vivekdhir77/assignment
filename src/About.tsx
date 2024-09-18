cd81bc70390818fe55d2ff7d956e4700bada0e1f
vivekdhir77 <vivekdhir77@gmail.com>
2024-09-17 21:17:51 -0400

    my crn and details

--- src/About.tsx
+++ src/About.tsx
@@ -2,7 +2,7 @@
 function About() {
   return (
       <div>
-        <p> <b> Built By: Sai Siddhartha Vivek Dhir Rangoju </b></p>
+        <p> <b> Built By: Sai Siddhartha Vivek Dhir Rangoju </b> Term: 202510 | CRN: 20596 | Section 3 <br /> <br /> Link to project: <a href="https://saisiddharthavivekdhirrangojua1.netlify.app">https://saisiddharthavivekdhirrangojua1.netlify.app</a> <br /> Project Repo: <a href="https://github.com/vivekdhir77/assignment">https://github.com/vivekdhir77/assignment</a></p>
       </div>
   );
 }
