9cf72b0855e9f355039e737202b80e77fc25524c
vivekdhir77 <vivekdhir77@gmail.com>
2024-10-03 20:43:35 -0400

    added route to labs and added logo

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -13,7 +13,11 @@ export default function KanbasNavigation() {
     <div id="wd-kanbas-navigation" style={{ width: 120 }} 
     className="list-group rounded-0
           position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank" className="list-group-item bg-black border-0 text-center">Northeastern</a><br/>
+<a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank" className="list-group-item bg-black border-0 text-center">
+    <img src="NU.png" alt="Northeastern University logo" style={{ width: "52px" }} />
+</a><br/>
+
+
       <Link to="/Kanbas/Account" id="wd-account-link" className="list-group-item text-center border-0 bg-black text-white">
       <FaRegCircleUser className="fs-1 text text-white" /><br />Account</Link><br/>
       <Link to="/Kanbas/Dashboard" id="wd-dashboard-link" className={location.pathname === '/Kanbas/Dashboard' ? activeStyle : defaultStyle}>
@@ -22,6 +26,6 @@ export default function KanbasNavigation() {
         <LiaBookSolid className="fs-1 text-danger" /><br />Courses</Link><br/>
       <Link to="/Kanbas/Calendar" id="wd-calendar-link" className={location.pathname === "/Kanbas/Calendar" ? activeStyle : defaultStyle}> <IoCalendarOutline className="fs-1 text-danger" /><br />Calendar</Link><br/>
       <Link to="/Kanbas/Inbox" id="wd-inbox-link"  className={location.pathname === "/Kanbas/Inbox" ? activeStyle : defaultStyle}> <FaInbox className="fs-1 text-danger" /><br />Inbox</Link><br/>
-      <Link to="Labs" id="wd-labs-link"  className={location.pathname === "/Kanbas/Labs" ? activeStyle : defaultStyle}> <LiaCogSolid className="fs-1 text-danger" /><br />Labs</Link><br/>
+      <Link to="/" id="wd-labs-link"  className={location.pathname === "/Kanbas/Labs" ? activeStyle : defaultStyle}> <LiaCogSolid className="fs-1 text-danger" /><br />Labs</Link><br/>
     </div>
 );}
