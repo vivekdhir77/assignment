df451fff3f0ce81ff7e70b285f00c9786ec6d8f2
vivekdhir77 <vivekdhir77@gmail.com>
2024-11-23 00:07:04 -0500

    pusing

--- kanbas-node-server-app/index.js
+++ kanbas-node-server-app/index.js
@@ -9,23 +9,16 @@ import ModuleRoutes from "./kanbas/Modules/routes.js";
 import EnrollmentRoutes from './kanbas/Enrollments/routes.js';
 import AssignmentRoutes from './kanbas/Assignments/routes.js';
 import "dotenv/config";
-
 const app = express();
-app.use(express.json());
-
-// Configure CORS to allow all origins
 app.use(cors({
-    origin: "*", // Allow all origins
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
 }));
-
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kanbas",
     resave: false,
     saveUninitialized: false,
 };
-
 if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
@@ -34,8 +27,8 @@ if (process.env.NODE_ENV !== "development") {
         domain: process.env.NODE_SERVER_DOMAIN,
     };
 }
-
 app.use(session(sessionOptions));
+app.use(express.json());
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
@@ -43,6 +36,4 @@ EnrollmentRoutes(app);
 AssignmentRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(process.env.PORT || 4000, () => {
-    console.log(`Server running on port ${process.env.PORT || 4000}`);
-});
+app.listen(process.env.PORT || 4000);
